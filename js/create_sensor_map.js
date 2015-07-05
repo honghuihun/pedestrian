c125d9c0d79c79d6f71b2ce7c5c2127392d09a1b
Jmily <weiyi.li713@gmail.com>
2015-07-05 10:47:25 +1000

    color

--- js/create_sensor_map.js
+++ js/create_sensor_map.js
@@ -8,13 +8,17 @@ var map2 = L.mapbox.map('sensor-map', 'cityscience.mk59l4j3')
 L.geoJson(sensorGeo,{
 
     pointToLayer: function (feature, latlng) {
+        console.log(feature.properties.sensorId);
+
+        var className = "sensorId"+feature.properties.sensorId;
         return L.circleMarker(latlng, {
             radius: 10,
             fillColor: "white",
             color: "#5964ff",
             weight: 1,
             opacity: 0.5,
-            fillOpacity: 1
+            fillOpacity: 1,
+            className: className
         });
     }
 }).addTo(map2);
