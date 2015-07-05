0a7b4e8b3e2c7101b88ecfec04864f223d02e0b0
Jmily <weiyi.li713@gmail.com>
2015-07-05 13:54:09 +1000

    hover

--- js/create_sensor_map.js
+++ js/create_sensor_map.js
@@ -5,12 +5,13 @@ var map2 = L.mapbox.map('sensor-map', 'cityscience.mk59l4j3')
     .setView([-37.8138506,144.9638644], 14);
 
 
+
 L.geoJson(sensorGeo,{
 
     pointToLayer: function (feature, latlng) {
         console.log(feature.properties.sensorId);
 
-        var className = "sensorId"+feature.properties.sensorId;
+        var className = "sensor sensorId"+feature.properties.sensorId;
         return L.circleMarker(latlng, {
             radius: 10,
             fillColor: "white",
