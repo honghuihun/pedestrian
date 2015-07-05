ff9d027faadedfadc5ba342b0861e97cc43fedbf
Jmily <weiyi.li713@gmail.com>
2015-07-05 15:12:07 +1000

    new layout

--- js/create_sensor_map.js
+++ js/create_sensor_map.js
@@ -1,9 +1,12 @@
 /**
  * Created by Julian on 5/07/15.
  */
-var map2 = L.mapbox.map('sensor-map', 'cityscience.mk59l4j3')
-    .setView([-37.8138506,144.9638644], 14);
+var map2 = L.mapbox.map('sensor-map', 'cityscience.mk59l4j3', {
+zoomControl: false
+}).setView([-37.8138506,144.9638644], 14);
 
+map2.doubleClickZoom.disable();
+map2.scrollWheelZoom.disable();
 
 
 L.geoJson(sensorGeo,{
@@ -13,12 +16,12 @@ L.geoJson(sensorGeo,{
 
         var className = "sensor sensorId"+feature.properties.sensorId;
         return L.circleMarker(latlng, {
-            radius: 10,
+            radius: 5,
             fillColor: "white",
             color: "#5964ff",
             weight: 1,
             opacity: 0.5,
-            fillOpacity: 1,
+            fillOpacity: 0.8,
             className: className
         });
     }
